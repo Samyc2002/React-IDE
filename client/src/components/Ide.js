375c65b1b91e2287646fc347dc7c2ba36b892f32
Samyc2002 <2000200402@iitdh.ac.in>
2022-10-12 19:23:24 +0530

    Added all themes

--- client/src/components/Ide.js
+++ client/src/components/Ide.js
@@ -29,9 +29,9 @@ import ImportFileButton from './ImportFileButton';
 
 function Ide({ value, onChange }) {
 
-    const theme = useTheme();
+    const Theme = useTheme();
     const isTabletorMobile = useMediaQuery('(max-width:800px)');
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const fullScreen = useMediaQuery(Theme.breakpoints.down('sm'));
 
     const handleChange = (editor, data, value) => {
         onChange(value);
@@ -45,6 +45,7 @@ function Ide({ value, onChange }) {
     const [openIn, setOpenIn] = React.useState(false);
     const [openOut, setOpenOut] = React.useState(false);
     const [input, setInput] = useState("");
+    const [theme, setTheme] = useState("material");
 
     const handlechange = (e) => {
         localStorage.setItem('lang', e.target.value);
@@ -60,6 +61,10 @@ function Ide({ value, onChange }) {
     const handleCloseIn = () => {
         setOpenIn(false);
     };
+
+    const handleTheme = (e) => {
+        setTheme(themes[e.target.value]);
+    }
 
     const handleCloseOut = () => {
         setOpenOut(false);
@@ -142,6 +147,58 @@ function Ide({ value, onChange }) {
     const lang_show = [ "C", "C++19", "C#", "Java", "Python3", "Ruby", "Kotlin", "Swift" ]
     const lang_store = [ "c", "cpp", "csharp", "java", "python3", "ruby", "kotlin", "swift" ]
 
+    const themes = [
+        'default',
+        '3024-day',
+        '3024-night',
+        'abcdef',
+        'ambiance',
+        'base16-dark',
+        'base16-light',
+        'bespin',
+        'blackboard',
+        'cobalt',
+        'colorforth',
+        'dracula',
+        'duotone-dark',
+        'duotone-light',
+        'eclipse',
+        'elegant',
+        'erlang-dark',
+        'hopscotch',
+        'icecoder',
+        'isotope',
+        'lesser-dark',
+        'liquibyte',
+        'material',
+        'mbo',
+        'mdn-like',
+        'midnight',
+        'monokai',
+        'neat',
+        'neo',
+        'night',
+        'panda-syntax',
+        'paraiso-dark',
+        'paraiso-light',
+        'pastel-on-dark',
+        'railscasts',
+        'rubyblue',
+        'seti',
+        'solarized dark',
+        'solarized light',
+        'the-matrix',
+        'tomorrow-night-bright',
+        'tomorrow-night-eighties',
+        'ttcn',
+        'twilight',
+        'vibrant-ink',
+        'xq-dark',
+        'xq-light',
+        'yeti',
+        'zenburn'
+    ];
+
     return (
         <div className="window">
             <div className="header">
@@ -150,6 +207,19 @@ function Ide({ value, onChange }) {
                 </Typography>
                 <div className='action-cont' style={{ paddingRight: '2vh' }}>
                     <ImportFileButton onChange={onChange} handleChange={handlechange} />
+                    <FormControl className="change-theme">
+                        <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="select-theme"
+                            value={theme}
+                            onChange={handleTheme}
+                        >
+                          {themes.map((t, id) => (
+                              <MenuItem value={id}>{t}</MenuItem>
+                          ))}
+                        </Select>
+                    </FormControl>
                     <FormControl className="change-language">
                         <InputLabel id="demo-simple-select-label">Language</InputLabel>
                         <Select
@@ -174,7 +244,7 @@ function Ide({ value, onChange }) {
                     lineWrapping: true,
                     lint: true,
                     mode: `${lang[language]}`,
-                    theme: 'material',
+                    theme: theme==="default" ? "" : theme,
                     lineNumbers: true,
                     extraKeys: {
                         'Cmd-/' : 'toggleComment',
